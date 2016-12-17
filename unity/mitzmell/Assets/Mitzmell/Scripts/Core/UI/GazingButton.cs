fec307fcdc9ce0b691faf1667530b6d702dc8d9a
chiepomme <chie@loves-miko.cc>
2016-12-17 20:09:05 +0900

    create timed gaze button

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/UI/GazingButton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Mitzmell
+{
+    class GazingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IGvrPointerHoverHandler
+    {
+        bool isGazing;
+        float gazingSeconds;
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            gazingSeconds = 0f;
+            isGazing = true;
+        }
+
+        public void OnGvrPointerHover(PointerEventData eventData)
+        {
+            if (!isGazing) return;
+
+            gazingSeconds += Time.deltaTime;
+            if (gazingSeconds < 1f) return;
+
+            print("gazed!");
+            isGazing = false;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isGazing = false;
+        }
+    }
+}
