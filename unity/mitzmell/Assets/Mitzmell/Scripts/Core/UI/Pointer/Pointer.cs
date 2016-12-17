8cc60a910d50269624837382ea2dd94e4c4bb5fd
chiepomme <chie@loves-miko.cc>
2016-12-17 20:09:46 +0900

    create vr pointer skeleton

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/UI/Pointer/Pointer.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace Mitzmell
+{
+    class Pointer : GvrBasePointer
+    {
+        public override float GetMaxPointerDistance() { return 50f; }
+
+        public override void GetPointerRadius(out float innerRadius, out float outerRadius)
+        {
+            innerRadius = 0.01f;
+            outerRadius = 0.1f;
+        }
+
+        public override void OnInputModuleDisabled()
+        {
+
+        }
+
+        public override void OnInputModuleEnabled()
+        {
+
+        }
+
+        public override void OnPointerClickDown()
+        {
+
+        }
+
+        public override void OnPointerClickUp()
+        {
+
+        }
+
+        public override void OnPointerEnter(GameObject targetObject, Vector3 intersectionPosition, Ray intersectionRay, bool isInteractive)
+        {
+
+        }
+
+        public override void OnPointerExit(GameObject targetObject)
+        {
+
+        }
+
+        public override void OnPointerHover(GameObject targetObject, Vector3 intersectionPosition, Ray intersectionRay, bool isInteractive)
+        {
+
+        }
+    }
+}
