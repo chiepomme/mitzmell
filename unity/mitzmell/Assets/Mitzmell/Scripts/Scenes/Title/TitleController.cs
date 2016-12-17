9b0465958b5b082a9514ef6957833b21e24dd611
chiepomme <chie@loves-miko.cc>
2016-12-17 16:41:30 +0900

    add title controller
    just a skeleton

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Scenes/Title/TitleController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mitzmell
+{
+    class TitleController : MonoBehaviour
+    {
+        [SerializeField]
+        float WaitSeconds;
+        [SerializeField]
+        Button GoNextButton;
+
+        IEnumerator Start()
+        {
+            AddButtonListeners();
+            yield return new WaitForSeconds(WaitSeconds);
+            GoNext();
+        }
+
+        void AddButtonListeners()
+        {
+            GoNextButton.onClick.AddListener(() => GoNext());
+        }
+
+        public void GoNext()
+        {
+            SceneTransit.TransitTo(Scenes.Title);
+        }
+    }
+}
