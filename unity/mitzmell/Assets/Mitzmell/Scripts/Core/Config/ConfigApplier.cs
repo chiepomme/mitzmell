2e533568bab340a66fff8c587da37af6148eaad4
chiepomme <chie@loves-miko.cc>
2016-12-17 21:38:38 +0900

    create configuration applier to set 3d vr mode

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/Config/ConfigApplier.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Mitzmell
+{
+    class ConfigApplier : MonoBehaviour
+    {
+        [SerializeField]
+        GvrViewer VRViewer;
+
+        void Start()
+        {
+            if (VRViewer != null) VRViewer.VRModeEnabled = Config.Is3D;
+        }
+    }
+}
