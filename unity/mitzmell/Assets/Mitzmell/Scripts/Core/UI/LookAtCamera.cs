dbf316668a9fbc44be78122dade864647f992fbe
chiepomme <chie@loves-miko.cc>
2016-12-17 20:07:22 +0900

    create look at camera component

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/UI/LookAtCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Mitzmell
+{
+    [ExecuteInEditMode]
+    class LookAtCamera : MonoBehaviour
+    {
+        void LateUpdate()
+        {
+            transform.LookAt(Camera.main.transform.position, Vector3.up);
+        }
+    }
+}
