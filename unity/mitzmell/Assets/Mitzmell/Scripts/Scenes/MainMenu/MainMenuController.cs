083b8d082857718b90d3456eb5269e56e8bf5305
chiepomme <chie@loves-miko.cc>
2016-12-17 17:05:11 +0900

    create main menu controller

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Scenes/MainMenu/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mitzmell
+{
+    class MainMenuController : MonoBehaviour
+    {
+        [SerializeField]
+        Button TwoDButton;
+        [SerializeField]
+        Button ThreeDButton;
+
+        void Start()
+        {
+            AddButtonListeners();
+        }
+
+        void AddButtonListeners()
+        {
+            TwoDButton.onClick.AddListener(() =>
+            {
+                Config.Is3D = false;
+                GoNext();
+            });
+
+            ThreeDButton.onClick.AddListener(() =>
+            {
+                Config.Is3D = true;
+                GoNext();
+            });
+        }
+
+        public void GoNext()
+        {
+            SceneTransit.TransitTo(Scenes.Title);
+        }
+    }
+}
