04b8e69908a0ab830580dba77719d278a7678e5b
chiepomme <chie@loves-miko.cc>
2016-12-18 15:03:38 +0900

    create StageInfo scriptable object

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/Stage/StageInfo.Editor.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Mitzmell
+{
+    partial class StageInfo : ScriptableObject
+    {
+        [MenuItem("Assets/Create/StageInfo")]
+        public static void CreateAsset()
+        {
+            CreateAsset<StageInfo>();
+        }
+
+        public static void CreateAsset<T>() where T : ScriptableObject
+        {
+            var info = CreateInstance<T>();
+            var path = AssetDatabase.GenerateUniqueAssetPath("Assets/" + typeof(T).Name + ".asset");
+
+            AssetDatabase.CreateAsset(info, path);
+            AssetDatabase.SaveAssets();
+
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = info;
+        }
+    }
+}
+#endif
---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/Stage/StageInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Mitzmell
+{
+    partial class StageInfo : ScriptableObject
+    {
+        public string Name;
+        public Texture SmallThumbnail;
+        public Texture LargeThumbnail;
+        public string AuthorUrl;
+        public string DownloadPath;
+    }
+}
