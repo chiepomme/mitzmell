b2aa53d85d365c9fbfc6bd0358aad20a546911f0
chiepomme <chie@loves-miko.cc>
2016-12-18 11:03:31 +0900

    create pass through button

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Core/UI/Button/PassThroughButton.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Mitzmell.UI
+{
+    class PassThroughButton : MonoBehaviour
+    {
+        [SerializeField]
+        public GazingButton LeftButton;
+        [SerializeField]
+        public GazingButton RightButton;
+
+        bool GazingLeft { get; set; }
+        bool GazingRight { get; set; }
+
+        public UnityEvent OnPassedToLeft = new UnityEvent();
+        public UnityEvent OnPassedToRight = new UnityEvent();
+
+        void Start()
+        {
+            AddButtonListeners();
+        }
+
+        void AddButtonListeners()
+        {
+            LeftButton.OnGazeStarted.AddListener(() =>
+            {
+                if (GazingRight)
+                {
+                    OnPassedToLeft.Invoke();
+                    GazingRight = false;
+                }
+
+                StopCoroutine("WaitAndFinishGazingLeft");
+                GazingLeft = true;
+            });
+
+            RightButton.OnGazeStarted.AddListener(() =>
+            {
+                if (GazingLeft)
+                {
+                    OnPassedToRight.Invoke();
+                    GazingLeft = false;
+                }
+
+                StopCoroutine("WaitAndFinishGazingRight");
+                GazingRight = true;
+            });
+
+            LeftButton.OnGazeEnded.AddListener(() =>
+            {
+                StopCoroutine("WaitAndFinishGazingLeft");
+                StartCoroutine("WaitAndFinishGazingLeft");
+            });
+
+            RightButton.OnGazeEnded.AddListener(() =>
+            {
+                StopCoroutine("WaitAndFinishGazingRight");
+                StartCoroutine("WaitAndFinishGazingRight");
+            });
+        }
+
+        IEnumerator WaitAndFinishGazingLeft()
+        {
+            yield return new WaitForSeconds(1f);
+            GazingLeft = false;
+        }
+
+        IEnumerator WaitAndFinishGazingRight()
+        {
+            yield return new WaitForSeconds(1f);
+            GazingRight = false;
+        }
+    }
+}
