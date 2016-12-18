0f9711eeb99e8926f9620009ffe86ab6f417a312
chiepomme <chie@loves-miko.cc>
2016-12-18 15:43:03 +0900

    create stage selector

---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Scenes/StageSelect/StageSelector/StageSelectorButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Mitzmell
+{
+    class StageSelectorButton : MonoBehaviour
+    {
+        [SerializeField]
+        GazingButton Button;
+        [SerializeField]
+        RawImage StageThumbnail;
+        [SerializeField]
+        StageInfo StageInfo;
+
+        public class StageSelectEvent : UnityEvent<StageInfo> { }
+        public StageSelectEvent OnSelected = new StageSelectEvent();
+
+        void Start()
+        {
+            Button.OnGazeStarted.AddListener(() => OnSelected.Invoke(StageInfo));
+
+            if (StageInfo != null)
+            {
+                StageThumbnail.texture = StageInfo.SmallThumbnail;
+            }
+        }
+    }
+}
---
+++ unity/mitzmell/Assets/Mitzmell/Scripts/Scenes/StageSelect/StageSelector/StageSelectorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mitzmell
+{
+    class StageSelectorController : MonoBehaviour
+    {
+        [SerializeField]
+        StageSelectorButton[] Buttons;
+        [SerializeField]
+        RawImage SelectedImage;
+        [SerializeField]
+        AudioSource PreviewSource;
+
+        void Start()
+        {
+            AddButtonListeners();
+        }
+
+        void AddButtonListeners()
+        {
+            foreach (var button in Buttons)
+            {
+                button.OnSelected.AddListener(stageInfo =>
+                {
+                    SelectedImage.texture = stageInfo.LargeThumbnail;
+                    PreviewSource.clip = stageInfo.PreviewClip;
+                    PreviewSource.Play();
+                });
+            }
+        }
+    }
+}
